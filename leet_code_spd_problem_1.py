cbfa1bf69bfa75364cb94af000c93c750d654675
Jayce Azua <jayce.azua@gmail.com>
2019-06-08 22:07:56 -0700

    done with two sum

--- leet_code_spd_problem_1.py
+++ leet_code_spd_problem_1.py
@@ -4,19 +4,19 @@
 
 class Solution:
     def twoSum(self, nums, target):
-      #       going to create a dictionary for instant lookup, where key is the number and the value is the index
-      # num_store = self.create_num_store(nums) # O(n) space complexity and time
-      num_store = {} # O(n) space complexity
+        #       going to create a dictionary for instant lookup, where key is the number and the value is the index
+        # num_store = self.create_num_store(nums) # O(n) space complexity and time
+        num_store = {}  # O(n) space complexity
 
-      # i want to have a bank of the numbers given to me
-      # as i go through the array of numbers I substract the given number with the target and get a value
-      # with that value number i will look in my bank of key(num) value(index) pairs
-      for index, num in enumerate(nums): #O(n) time complexity
-          lookup = target - num
-          if lookup in num_store: # O(1) time "lookup" complexity
-              return [num_store[lookup], index]
-          num_store[num] = index
-      return []
+        # i want to have a bank of the numbers given to me
+        # as i go through the array of numbers I substract the given number with the target and get a value
+        # with that value number i will look in my bank of key(num) value(index) pairs
+        for index, num in enumerate(nums):  # O(n) time complexity
+            lookup = target - num
+            if lookup in num_store:  # O(1) time "lookup" complexity
+                return [num_store[lookup], index]
+            num_store[num] = index
+        return []
 
 
 # Restate: Okay, so I am given an array that contains a list of numbers.
@@ -47,3 +47,5 @@ class Solution:
 # if it does not find it it will shift the pointer 1 up one and the pointer 2 will restart to pointer's 1 index plus 1 and repeat
 # until it finds the total sum to return the index. If not it will return an empty list.
 # However this solution is O(n^2) in time complexity and O(1) space complexity. Meaning it will take a long time if we get large inputs
+
+# Leetcode: https://leetcode.com/submissions/detail/234633810/
